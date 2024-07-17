8d80018b618336ed2759e16d097165bcbdb63cf6
SucharitaGorai <sucharitagorai08@gmail.com>
2024-07-17 20:41:47 +0530

    first commit

---
+++ frontend/src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='d-flex justify-content-center align-items-center vh-100' style={{ backgroundImage: 'url(background.jpg)', backgroundSize: 'cover' }}>
+    <div className='bg-white p-3 rounded w-25' style={{ border: '2px solid #ccc', borderRadius: '10px' }}>
+     
+              <h2>Welcome Again!!!</h2>
+              </div>
+        </div>
+    );
+}
+export default Home
